66dccc1087940062c568fd2715062414ca32ce43
OlegShevtsiv <oleg28skm@gmail.com>
2020-09-23 11:05:52 +0300

    Create init ASP Web API template

---
+++ IsraelIT_test/IsraelIT_test/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IsraelIT_test.Controllers
+{
+    [ApiController]
+    [Route("Book")]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public object Get(int id = 0) 
+        {
+            return $"This is book with id {id}";
+        }
+
+        /// <summary>
+        /// This is API action
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("/GetAll")]
+        public string GetAll() 
+        {
+            return "Hello";
+        }
+    }
+}
